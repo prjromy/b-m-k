522744b81d0b964529e25f64040bad938f24bc81
Romy <prjromy@gmail.com>
2020-11-19 19:57:14 +0545

    material module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+
+
+
+
+const MaterialComponents=[
+  
+  MatButtonModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    
+    MaterialComponents
+  ],
+  exports:[
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
